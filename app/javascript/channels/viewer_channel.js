425d1bd09967709c6445551dd7b11f1c64ede5d4
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-08-04 13:22:10 -0700

    push

--- app/javascript/channels/viewer_channel.js
+++ app/javascript/channels/viewer_channel.js
@@ -12,13 +12,7 @@ consumer.subscriptions.create("ViewerChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    $.post("/users/inviewer", {user: data.content.user, room: data.content.room})
-    
-    $(document).ready(() => {
-      $.get("/conversations/attendees", {room: urlParams.get('chatrm'), user: "<%= current_user.id %>", owner: "<%= @user.id %>"}, (data) => {
-        if (data.success == true)
-          $('#attendees').html(data.html)
-      })
-    })
+    alert("new user joined")
+
   }
 });
