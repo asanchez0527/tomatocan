4bc6ba885da2b13c656b9a979b123fe087c4257c
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-08-04 22:42:59 -0700

    update to credentials

--- config/initializers/stripeapikey.rb
+++ config/initializers/stripeapikey.rb
@@ -2,6 +2,6 @@ if Rails.env.development? || Rails.env.test?
   Stripe.api_key = STRIPE_SECRET_KEY
   STRIPECONNECTCLIENTID = STRIPE_CONNECT_CLIENT_ID
 else
-  Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-  STRIPECONNECTCLIENTID = ENV['STRIPE_CONNECT_CLIENT_ID'] 
+  Stripe.api_key = Rails.application.credentials.STRIPE_SECRET_KEY
+  STRIPECONNECTCLIENTID = Rails.application.credentials.STRIPE_CONNECT_CLIENT_ID
 end
