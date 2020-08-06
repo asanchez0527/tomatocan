31a7a51628a5173db0080298c6dc416d83497e16
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-08-06 13:30:25 -0700

    fix bug with attendees

--- app/javascript/packs/viewer.js
+++ app/javascript/packs/viewer.js
@@ -9,9 +9,18 @@ $(document).ready(() => {
   room = $("#user_vars").data("room")
 
   $.post("/users/load", { room: room, user: current_user, owner: owner })
+
+  loadAttendees()
 })
+
+function loadAttendees() {
+  $.get("/conversations/attendees", {user: current_user, owner: owner, room: room}, (data) => {
+    $("#attendees").html(data.html)
+  })
+}
 
 $(window).on('beforeunload', () => {
   $.post("/users/unload", { user: current_user, room: room })
   console.log("unloaded")
 });
+
